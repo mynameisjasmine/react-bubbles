185997502748c7da7938ee630ac2fd18afc46001
Jasmine <jasminecharles@hotmail.com>
2019-10-18 09:21:49 -0700

    routes, login working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 import Login from "./components/Login";
 import "./styles.scss";
 
@@ -8,11 +9,23 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+      <ul className="links">
+        <li>
+        <Link to="/">Login</Link>
+        </li>
+        <li>
+        <Link to="/private">Private Page</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated.
         */}
+        <PrivateRoute path="/private" component={BubblePage}/>
+        <Route component={Login}/>
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Redirect } from 'react-router-dom';
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [input, setInput] = useState({username: '', password: ''})
+
+  const handleChange = event => {
+    setInput({
+    ...input,
+    [event.target.name]: event.target.value
+    })
+  
+  }
+
+
+  const login = event => {
+    event.preventDefault()
+  
+    axiosWithAuth()
+    .post('/api/login', input)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload)
+      props.history.push('/private')
+    })
+    .catch(err => console.log(err.response))
+  };
+
+  if (localStorage.getItem('token')) {
+    return <Redirect to='private' />
+  }
+
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+  <h1 className="welcome">Welcome to the Bubble App!</h1>
+  
+  <form className="form" onSubmit={login}>
+  <input 
+  type='text'
+  name='username'
+  value={input.username}
+  placeholder='...enter username'
+  onChange={handleChange}
+  />
+  <input 
+  type='password'
+  name='password'
+  value={input.password}
+  placeholder='...enter password'
+  onChange={handleChange}
+  />
+  <button>Log In</button>
+      </form>
     </>
   );
 };
